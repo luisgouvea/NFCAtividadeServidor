323227b8444c7e20d224582942ed0a6499ba5fab
Eduardo Silva <luiseduardo.gouvea@hotmail.com>
2017-10-21 23:01:52 -0200

    Ajuste de rotas e criacao de Controller

---
+++ NFCAtividadeServidor/NFCAtividadeAPI/Controllers/TesteComunicacaoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NFCAtividadeAPI.Controllers
+{
+    public class TesteComunicacaoController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Comunicar()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Funcionou a comunicacao");
+        }
+    }
+}
