20fd02f7cbe4055f103f7d90bc391dc4bca6a80a
Eduardo Silva <luiseduardo.gouvea@hotmail.com>
2017-11-01 13:15:38 -0200

    Criando mock / publicacao

---
+++ NFCAtividadeServidor/NFCAtividadeAPI/Models/Atividade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NFCAtividadeAPI.Models
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public List<TAG> listTag { get; set; }
+    }
+}
